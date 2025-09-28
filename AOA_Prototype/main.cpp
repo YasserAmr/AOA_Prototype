d2139964a8ce3d0bf85139a71ae7097051486fb4
YasserAmr <49810021+YasserAmr@users.noreply.github.com>
2025-09-28 15:30:10 +0200

    Add <QQuickStyle> to the project

--- AOA_Prototype/main.cpp
+++ AOA_Prototype/main.cpp
@@ -1,9 +1,12 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQuickStyle>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+
+    QQuickStyle::setStyle("Basic");
 
     QQmlApplicationEngine engine;
     QObject::connect(
